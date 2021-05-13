1c78aeb9c9d7e6e2c5788ca6c3f96a8219cace65
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-12 22:02:52 -0600

    Lint deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,18 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
-
-const Turn = require('../src/Turn');
-const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Deck', function() {
 
   it('should be a function', function() {
 
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    expect(Deck).to.be.a('function');
   });
-})
+});
