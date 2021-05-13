5a85b592923b06d47945c54994cb546a58b78b81
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-13 10:04:38 -0600

    Fix game parameter fully

--- test/Game-test.js
+++ test/Game-test.js
@@ -4,6 +4,7 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Game = require('../src/Game');
+const Card = require('../src/Card');
 
 const data = require('../src/data');
 const prototypeQuestions = data.prototypeData;
@@ -17,11 +18,33 @@ describe('Game', function() {
 
   it('should keep track of currentRound', function() {
 
-    const deck = new Deck(["card1", "card2", "card3"])
-    const round = new Round(deck);
-    const game = new Game(round);
+  function createCard(id, question, answers, correctAnswer) {
+    const card = new Card(id, question, answers, correctAnswer);
+    return card;
+  }
 
-    expect(game.currentRound).to.equal(round);
+    for (let i = 0; i < prototypeQuestions.length; i++) {
+
+      let newCardArray = [];
+
+      // const {id, question, answers, correctAnswer} = prototypeQuestions[i];
+
+      const newCard = createCard(
+        prototypeQuestions[i].id,
+        prototypeQuestions[i].question,
+        prototypeQuestions[i].answers,
+        prototypeQuestions[i].correctAnswer
+      );
+      newCardArray.push(newCard);
+    }
+
+    let deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+    const game = new Game();
+
+    game.start();
+
+    expect(game.currentRound).to.deep.equal(round);
   });
 
   it('should create Cards, put them in a deck, & create new round', function() {
