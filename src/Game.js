d03e103ca1379879eac513b19975ff213ed0c90a
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-13 16:08:25 -0600

    Lint game

--- src/Game.js
+++ src/Game.js
@@ -5,7 +5,6 @@ const util = require('./util');
 const Card = require('./Card');
 const Round = require('./Round');
 const Deck = require('./Deck');
-const Turn = require('./Turn');
 
 class Game {
   constructor() {
@@ -23,14 +22,13 @@ class Game {
 
       let newCardArray = [];
 
-      // const {id, question, answers, correctAnswer} = prototypeQuestions[i];
-
       const newCard = this.createCard(
         prototypeQuestions[i].id,
         prototypeQuestions[i].question,
         prototypeQuestions[i].answers,
-        prototypeQuestions[i].correctAnswer);
-        newCardArray.push(newCard);
+        prototypeQuestions[i].correctAnswer
+      );
+      newCardArray.push(newCard);
     }
 
     let deck = new Deck(prototypeQuestions);
@@ -42,7 +40,9 @@ class Game {
   }
 
   printMessage(deck, round) {
-    console.log(`Welcome to FlashCards! You are playing with ${this.currentRound.deck.countCards()} cards.
+    console.log(`Welcome to FlashCards! You are playing with ${
+      this.currentRound.deck.countCards()
+    } cards.
 -----------------------------------------------------------------------`)  
   }
 
