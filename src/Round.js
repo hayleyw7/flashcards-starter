329e71779d6e75de0651fa3a7570ba55c1283658
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-13 16:20:58 -0600

    Lint round test

--- src/Round.js
+++ src/Round.js
@@ -33,7 +33,7 @@ class Round {
 
   endRound() {
     console.log(`** Round over! ** You answered ` + this.calculatePercentCorrect() + `% of the questions correctly!`)
-    return `** Round over! ** You answered ` + this.calculatePercentCorrect() + `% of the questions correctly!`
+    return `** Round over! ** You answered ` + this.calculatePercentCorrect() + `% of the questions correctly!`;
   }
 }
 
