b5d69b71a097edfa5379b408825386e9adc2fd95
Hayley Witherell <hayleywitherell@gmail.com>
2021-05-13 20:39:04 -0600

    Add fat arrows

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,9 +2,9 @@ const chai = require('chai');
 const expect = chai.expect;
 const Deck = require('../src/Deck');
 
-describe('Deck', function() {
+describe('Deck', () => {
 
-  it('should be a function', function() {
+  it('should be a function', () => {
 
     expect(Deck).to.be.a('function');
   });
